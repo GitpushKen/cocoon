43622f2d434ff045226c57258053ce096bb3727b
GitpushKen <kendivers@outlook.fr>
2024-02-16 00:34:01 +0100

    1.1

---
+++ app/js/scriptEZ.js
@@ -0,0 +1,191 @@
+let bootAudio = new Audio("../assets/sounds/bootup.mp3")
+let bootUp = document.getElementById("bootUp")
+let miniMe = document.querySelector(".miniMe")
+let titled = document.querySelector(".titled")
+
+
+let bootEnd = false;
+
+
+window.onload = (e) => {
+    
+        boot();
+    
+}
+
+function boot() {
+    bootAudio.play();
+    bootAudio.volume = 0.20;
+    bootEnd = true;
+    miniMe.classList.toggle("active");
+    titled.classList.toggle("active");
+    console.log(bootEnd)
+}
+
+
+setTimeout(function(){bootUp.classList.toggle("inactive")},2000);
+
+function started() {
+    bootUp.classList.toggle("inactive");
+    bootEnd = false;
+    console.log(bootEnd);
+}
+
+
+// Menu principal
+
+let startBtn = document.getElementById("start")
+let titleScreen = document.querySelector(".main")
+let mainMenu = document.querySelector(".mainMenu")
+
+startBtn.addEventListener("click", start)
+
+function start() {
+    miniMe.classList.toggle("inactive");
+    titled.classList.toggle("inactive");
+    setTimeout(function(){titleScreen.classList.toggle("inactive")},550);
+    setTimeout(function(){mainMenu.classList.toggle("active")},550);
+    setTimeout(startTime(), 550);
+}
+
+// Menu principal -- Blocks
+
+const who = document.getElementById("who");
+const what = document.getElementById("what");
+const how = document.getElementById("how");
+
+const contents = document.querySelector(".contents")
+const pres = document.querySelector(".presScroller")
+
+
+let whoOn = false;
+let whatOn = false;
+let howOn = false;
+
+
+who.addEventListener("click", toggleWho);
+what.addEventListener("click", toggleWhat);
+how.addEventListener("click", toggleHow);
+
+
+function displayPres() {
+    pres.classList.toggle("active")
+    setTimeout(pres.style.display = "flex", 550);
+}
+
+function whatOff() {
+    what.classList.remove("active");
+    what.classList.toggle("down");
+    setTimeout(what.style.display = "none", 550);
+}
+
+function howOff() {
+    how.classList.remove("active");
+    how.classList.toggle("down");
+    setTimeout(how.style.display = "none", 550);
+}
+
+function toggleWho() {
+    if (!whoOn) {   
+        who.classList.toggle("active");
+        displayPres();
+        whatOff();
+        howOff();
+
+        whoOn = true;
+        console.log("who", whoOn);
+
+    } else {
+        what.classList.remove("down");
+        setTimeout(what.style.display = "flex", 550);
+        how.classList.remove("down");
+        setTimeout(how.style.display = "flex", 550);
+        who.classList.remove("active");
+        pres.classList.remove("active");
+        setTimeout(pres.style.display = "none", 550);
+        whoOn = false;
+        console.log("who",whoOn);
+    }
+
+    
+    
+}
+
+
+
+function toggleWhat() {
+    if (!whatOn) {
+        what.classList.toggle("active");
+        how.classList.toggle("down");
+        whatOn = true;
+        console.log("what", whatOn);
+    } else {
+        how.classList.remove("down");
+        what.classList.remove("active");
+        whatOn = false;
+        console.log("what", whatOn); 
+    }
+    
+    /*if (whoOn) {
+        how.classList.remove("down");
+    } else {
+        how.classList.toggle("down")
+    }*/
+
+    who.classList.remove("active");
+    how.classList.remove("active");
+}
+function toggleHow() {
+    how.classList.toggle("active");
+    howOn = true;
+
+    who.classList.remove("active");
+    what.classList.remove("active");
+}
+
+// barre d'état
+
+
+function startTime() {
+    let today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    m = checkTime(m);
+    document.getElementById('time').innerHTML =
+    h + ":" + m;
+  }
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+  };
+
+// music 
+
+const play = document.getElementById("lofi");
+let audio = new Audio("../assets/sounds/Lofi-sample.mp3")
+let music = false; 
+
+document.getElementById('lofi').innerHTML =
+    "music : Off";
+
+function playMusic() {
+    if (!music) {;
+    audio.play()
+    audio.volume = 0.45;
+    music = true;
+    console.log(music)
+    play.classList.toggle("active")
+    document.getElementById('lofi').innerHTML =
+    "music : On";
+
+    } else { 
+        audio.pause()
+        music = false;
+        play.classList.remove("active")
+        document.getElementById('lofi').innerHTML =
+    "music : Off";
+
+    }
+}
+
+play.addEventListener("click", playMusic)
