0a8b5b90d73652d28df39eb67300aadc3bdbd98a
Ken P <kendivers@outlook.fr>
2024-03-08 09:58:34 +0100

    Fix

--- app/js/mobile-script.js
+++ app/js/mobile-script.js
@@ -6,9 +6,9 @@ const menu = document.querySelector(".container-menu");
 
 const block = document.querySelector(".block");
 
-const whoBtn = document.getElementById("who");
-const whatBtn = document.getElementById("what");
-const howBtn = document.getElementById("how");
+const whoBtn = document.getElementById("whoT");
+const whatBtn = document.getElementById("whatT");
+const howBtn = document.getElementById("howT");
 
 const whoBlock = document.getElementById("whoB");
 const whatBlock = document.getElementById("whatB");
@@ -17,9 +17,10 @@ const howBlock = document.getElementById("howB");
 const backBtn = document.getElementById("backTo");
 
 
-let who = false;
-let what = false;
-let how = false;
+
+let whoT = false;
+let whatT = false;
+let howT = false;
 
 
 clickSound = new Audio("assets/sounds/click.mp3")
@@ -81,7 +82,7 @@ function whoClicked() {
     }
 
 
-    if (!who) {
+    if (!whoT) {
         whoBtn.classList.add("active")
         whoBlock.classList.toggle("visible")
         whatBlock.classList.add("hidden")
@@ -92,14 +93,14 @@ function whoClicked() {
         whatBtn.classList.add("hidden")
         howBtn.classList.add("hidden")
 
-        who = true;
+        whoT = true;
     } else {
         whoBtn.classList.remove("active")
         whoBlock.classList.remove("visible")
         whatBtn.classList.remove("hidden")
         howBtn.classList.remove("hidden")
         backBtn.classList.remove("hidden")
-        who = false;
+        whoT = false;
     }
 }
 
@@ -123,7 +124,7 @@ function whatClicked() {
 
         howBtn.classList.add("hidden")
 
-        what = true;
+        whatT = true;
     } else {
         whatBtn.classList.remove("active")
         whatBlock.classList.remove("visible")
@@ -131,7 +132,7 @@ function whatClicked() {
         backBtn.classList.remove("hidden")
 
         howBtn.classList.remove("hidden")
-        what = false;
+        whatT = false;
     }
 }
 
@@ -142,20 +143,20 @@ function howClicked() {
         clickSound.volume = 0.20;
     }
 
-    if (!how) {
+    if (!howT) {
         howBtn.classList.add("active")
         howBlock.classList.toggle("visible")
         howBlock.classList.remove("hidden")
 
 
-        how = true;
+        howT = true;
     } else {
         howBtn.classList.remove("active")
         howBlock.classList.remove("visible")
         howBlock.classList.add("hidden")
 
 
-        how = false;
+        howT = false;
     }
 }
 
@@ -239,3 +240,342 @@ function sendMail(){
     window.location.href = "mailto:kenperot@outlook.fr?subject=En%20savoir%20plus&body=";
     console.log("work")
 }
+//// Toggle btn
+
+const boxScrolled = document.querySelectorAll(".box");
+
+const welcomeBtn = document.querySelector(".welcome-screen")
+const btnDown = document.getElementById("enter")
+const homeBtn = document.getElementById("link1")
+
+const navBar = document.querySelector(".nav-bar")
+
+const navBtns = document.querySelectorAll(".nav-bar ul li a");
+const nav1Scrolled = document.getElementById("link1") 
+const nav2Scrolled = document.getElementById('link2')
+const nav3Scrolled = document.getElementById('link3')
+  
+const aboutPage = document.querySelectorAll(".about")
+const contactPage = document.querySelectorAll(".contact-me")
+
+const panel1 = document.querySelector(".sub-cont1")
+const panel2 = document.querySelector(".sub-cont2")
+const panel3 = document.querySelector(".sub-cont3")
+
+const who = document.getElementById("who");
+const what = document.getElementById("what");
+const how = document.getElementById("how");
+
+const contents = document.querySelector(".contents")
+const pres = document.querySelector(".presScroller")
+const scroller = document.querySelector(".scroller")
+
+
+let whoOn = false;
+let whatOn = false;
+let howOn = false;
+
+
+who.addEventListener("click", toggleWho);
+what.addEventListener("click", toggleWhat);
+how.addEventListener("click", toggleHow);
+
+
+function toggleWho() {
+    if (soundMenu) {
+        clickSound.play();
+        clickSound.volume = 0.20;
+    }
+    if (!whoOn) {   
+        who.classList.toggle("active");
+        pres.classList.toggle("active");
+        what.classList.toggle("down");
+        how.classList.toggle("down");
+        whoOn = true;
+        console.log("who", whoOn);
+        scroller.scrollTop = 0; // Permet de reset le scroll
+        
+
+    } else {
+        what.classList.remove("down");
+        how.classList.remove("down");
+        how.classList.remove("down2");
+        who.classList.remove("active");
+        pres.classList.remove("active")
+        
+        whoOn = false;
+        console.log("who",whoOn);
+    }
+
+    if (howOn) {
+        iconMail2.classList.remove("active");
+    }
+    backBtn.classList.toggle("inactive");
+    how.classList.remove("active");
+    what.classList.remove("active");
+    panelPro.classList.remove("active")
+}
+
+function toggleWhat() {
+
+    if (soundMenu) {
+        clickSound.play();
+        clickSound.volume = 0.20;
+    }
+    if (!whatOn) {
+        what.classList.toggle("active");
+        panelPro.classList.toggle("active")
+        how.classList.toggle("down2");
+        whatOn = true;
+        console.log("what", whatOn);
+    } else {
+        how.classList.remove("down2");
+        what.classList.remove("active");
+        panelPro.classList.remove("active")
+        whatOn = false;
+        console.log("what", whatOn); 
+    }
+
+    if (howOn) {
+        iconMail2.classList.remove("active");
+    }
+    
+    /*if (whoOn) {
+        how.classList.remove("down");
+    } else {
+        how.classList.toggle("down")
+    }*/
+    backBtn.classList.toggle("inactive");
+    who.classList.remove("active");
+    how.classList.remove("active");
+}
+
+function toggleHow() {
+    if (soundMenu) {
+        clickSound.play();
+        clickSound.volume = 0.20;
+    }
+    how.classList.toggle("active");
+    iconMail2.classList.toggle("active");
+    howOn = true;
+    who.classList.remove("active");
+    what.classList.remove("active");
+    backBtn.classList.toggle("inactive");
+
+}
+
+//// barre d'état
+
+
+
+
+//// Mail 
+
+const iconMail = document.querySelector(".icons")
+
+iconMail.addEventListener('click', sendMail)
+
+function sendMail(){
+    window.location.href = "mailto:kenperot@outlook.fr?subject=En%20savoir%20plus&body=";
+    console.log("work")
+}
+
+
+
+
+const iconMail2 = document.querySelector(".contact")
+const panelPro = document.querySelector(".panelProject")
+
+iconMail2.addEventListener('click', sendMail)
+const who = document.getElementById("who");
+const what = document.getElementById("what");
+const how = document.getElementById("how");
+
+const contents = document.querySelector(".contents")
+const pres = document.querySelector(".presScroller")
+const scroller = document.querySelector(".scroller")
+
+
+let whoOn = false;
+let whatOn = false;
+let howOn = false;
+
+
+who.addEventListener("click", toggleWho);
+what.addEventListener("click", toggleWhat);
+how.addEventListener("click", toggleHow);
+
+
+function toggleWho() {
+    if (soundMenu) {
+        clickSound.play();
+        clickSound.volume = 0.20;
+    }
+    if (!whoOn) {   
+        who.classList.toggle("active");
+        pres.classList.toggle("active");
+        what.classList.toggle("down");
+        how.classList.toggle("down");
+        whoOn = true;
+        console.log("who", whoOn);
+        scroller.scrollTop = 0; // Permet de reset le scroll
+        
+
+    } else {
+        what.classList.remove("down");
+        how.classList.remove("down");
+        how.classList.remove("down2");
+        who.classList.remove("active");
+        pres.classList.remove("active")
+        
+        whoOn = false;
+        console.log("who",whoOn);
+    }
+
+    if (howOn) {
+        iconMail2.classList.remove("active");
+    }
+    backBtn.classList.toggle("inactive");
+    how.classList.remove("active");
+    what.classList.remove("active");
+    panelPro.classList.remove("active")
+}
+
+function toggleWhat() {
+
+    if (soundMenu) {
+        clickSound.play();
+        clickSound.volume = 0.20;
+    }
+    if (!whatOn) {
+        what.classList.toggle("active");
+        panelPro.classList.toggle("active")
+        how.classList.toggle("down2");
+        whatOn = true;
+        console.log("what", whatOn);
+    } else {
+        how.classList.remove("down2");
+        what.classList.remove("active");
+        panelPro.classList.remove("active")
+        whatOn = false;
+        console.log("what", whatOn); 
+    }
+
+    if (howOn) {
+        iconMail2.classList.remove("active");
+    }
+    
+    /*if (whoOn) {
+        how.classList.remove("down");
+    } else {
+        how.classList.toggle("down")
+    }*/
+    backBtn.classList.toggle("inactive");
+    who.classList.remove("active");
+    how.classList.remove("active");
+}
+
+function toggleHow() {
+    if (soundMenu) {
+        clickSound.play();
+        clickSound.volume = 0.20;
+    }
+    how.classList.toggle("active");
+    iconMail2.classList.toggle("active");
+    howOn = true;
+    who.classList.remove("active");
+    what.classList.remove("active");
+    backBtn.classList.toggle("inactive");
+
+}
+
+//// barre d'état
+
+
+
+
+//// Mail 
+
+const iconMail = document.querySelector(".icons")
+
+iconMail.addEventListener('click', sendMail)
+
+function sendMail(){
+    window.location.href = "mailto:kenperot@outlook.fr?subject=En%20savoir%20plus&body=";
+    console.log("work")
+}
+
+
+
+
+const iconMail2 = document.querySelector(".contact")
+const panelPro = document.querySelector(".panelProject")
+
+iconMail2.addEventListener('click', sendMail)
+
+
+btnDown.addEventListener("click", toggleBtn)
+homeBtn.addEventListener("click", toggleHome)
+
+function toggleBtn() {
+    nav1Scrolled.classList.add("active")
+    navBar.classList.add("active")
+    panel1.style.display="block";
+    panel2.style.display="none";
+    panel3.style.display="none";
+    welcomeBtn.style.display="none";
+    soundToggle.volume = 0.50;
+        soundToggle.play();
+}
+
+
+//// navbar
+
+navBtns.forEach(item => {
+    item.addEventListener("click", function (m) {
+        soundToggle.volume = 0.50;
+        soundToggle.play();
+    })
+    item.addEventListener("click", function(e) {
+        for(let i = 0; i < navBtns.length; i++) {
+            navBtns[i].classList.remove("active");
+        }
+        this.classList.add("active");
+    });
+});
+
+function toggleHome() {
+    panel1.style.display="block";
+    panel2.style.display="none";
+    panel3.style.display="none";
+    welcomeBtn.style.display="none";
+}
+
+function toggleAbout() {
+    panel2.classList.add("visible")
+    panel2.style.display="block";
+    panel1.style.display="none";
+    panel3.style.display="none";
+    panel3.classList.remove("visible")
+    welcomeBtn.style.display="none";
+}
+
+function toggleContact() {
+    panel3.classList.add("visible")
+    panel2.classList.remove("visible")
+    panel3.style.display="block";
+    panel2.style.display="none";
+    panel1.style.display="none";
+    welcomeBtn.style.display="none";
+}
+
+nav2Scrolled.addEventListener("click", toggleAbout)
+nav2Scrolled.addEventListener("click", toggleAbout)
+nav3Scrolled.addEventListener("click", toggleContact)
+
+
+let soundToggle = new Audio("assets/sounds/switch.wav")
+
+
+
+
